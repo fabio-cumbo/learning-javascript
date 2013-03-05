8d96995057e6c024f4004ecd11d7103ea8745c7e
Fabio Cumbo <fabio.cumbo@hotmail.it>
2013-03-05 20:41:52 +0100

    added exercise0X.js

--- 2013-03-04/exercise03.js
+++ 2013-03-04/exercise03.js
@@ -1,23 +1,21 @@
 console.log("EXERCISE01");
 
  var mat = "";
+ var diag = 1;
+ var oldDiag = 1;
  for (var i=1; i<=10; i+=1) {
  	for (var j=1; j<=10; j+=1) {
  		if (i===j) {
- 			if (i===10) {
- 				mat += "1\t";
- 			}
- 			else {
+ 			if (i===10)
+ 				mat += "1";
+ 			else
  				mat += "1,\t";
- 			}
  		}
  		else {
- 			if (i===10) {
-			 	mat += "0\t";
-			}
-			else {
-				mat += "0,\t";
- 			}
+ 			if (j==10)
+ 				mat += "0";
+ 			else
+ 				mat += "0,\t";
  		}
  	}
  	mat += "\n";
