d1a9b25a995fa4d11fa8628518d02ffa321d42f7
Fabio Cumbo <fabio.cumbo@gmail.com>
2018-01-25 14:59:47 -0500

    Update exercise01.js

--- 2013-03-08/exercise01.js
+++ 2013-03-08/exercise01.js
@@ -1,21 +1,23 @@
 console.log("EXERCISE01");
+
 function functionNumber1(n) {
 	var arr = [];
 	for (var i = 1; i<=n; i++) {
 		arr.push(i);
 	}
+	
 	arr = arr.filter(function(item){
 		return (item % 2 === 0);
-		});
-arr = arr.map(function(item){
+	});
+	arr = arr.map(function(item){
 		return item * 2;
-		});
-arr = arr.filter(function(item){
+	});
+	arr = arr.filter(function(item){
 		return (item % 4 === 0);
-   		});
-arr = arr.reduce(function(prev, cur){
+   	});
+	arr = arr.reduce(function(prev, cur){
 		return prev + cur;
-		});
+	});
 
-return arr;
+	return arr;
 }
